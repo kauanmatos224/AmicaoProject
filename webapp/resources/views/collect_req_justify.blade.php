6574b9306a903412d1d54176d3f0f1548d58d690
VictorRayan <victorrayansouzaramos@gmail.com>
2022-11-03 10:18:34 -0300

    melhorado o gerenciamento de requisições

--- webapp/resources/views/collect_req_justify.blade.php
+++ webapp/resources/views/collect_req_justify.blade.php
@@ -1,119 +1,127 @@
 <!doctype html>
 <html lang="pt-BR">
-    <head><title>Justificativa - Requisição</title>
-    @include("./user_routesNavigation")</head>
+    <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+			  rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/style/estiloInsp.css"/>
+        <title>Justificativa - Requisição</title>
+        @include("./user_routesNavigation")
+</head>
     <body onload="onloadPage()">
-
-        <form id="frm_collect_justify" method="post" action="/institucional/requisicoes/doAction">
-            <?php 
-            
-                if(isset($info)){
-                    if($info=='wrong_justify'){
-                        echo '<p>Uma justificativa válida deve ser informada para realizar a alteração da requisição.</p>';
+        <div id ="div1">
+            <form id="frm_collect_justify" method="post" action="/institucional/requisicoes/doAction">
+                <?php 
+                
+                    if(isset($info)){
+                        if($info=='wrong_justify'){
+                            echo '<p>Uma justificativa válida deve ser informada para realizar a alteração da requisição.</p>';
+                        }
+                        else if($info=='wrong_date'){
+                            echo '<p>Forneça uma data válida.</p>';
+                        }
+                        else if($info=='wrong_none_date_set'){
+                            echo '<p>A data de agendamento permaneceu a mesma, portanto a requisição não foi alterada.</p>';
+                        }
                     }
-                    else if($info=='wrong_date'){
-                        echo '<p>Forneça uma data válida.</p>';
+                
+                ?>
+                <input type="hidden" name="_token" value="{{{csrf_token()}}}">
+                <input type="hidden" name="op_type" value="<?= $op_type ?>">
+                <input type="hidden" name="_id" value="<?= $id ?>">
+                <span>Justificativa:</span><br>
+                <textarea id="txtJustify" name="txtJustify" rows="4" cols="50"></textarea><br><br>
+
+                <p>Data de agendamento: <?php echo date('d/m/Y H:i:s', $datetime); ?></p><br>
+                <?php 
+                
+                
+                    if($op_type=='change'){
+                        echo 'Alterar Data<input type="radio" name="txtRadio" id="txtRadio" onclick="ChangeDate()"><br>
+                            Cancelar alteração<input type="radio" name="txtRadioResetDate" id="txtRadioResetDate" onclick="CancelDate()"><br>';
+                        echo '<input type="datetime-local" id="txtDatetime" name="txtDatetime" onChange="onRestoreDateInfo()">';
+                        echo '<input type="hidden" id="txtDate_info_obj" name="txtDate_info_obj" value="resetted">';
+                        echo '<input type="hidden" id="txtTimestamp" name="txtTimestamp" value="'.$datetime.'">';
+
+                        
+
+                        echo '<script type="text/javascript">
+                        
+                                function ChangeDate(){
+                                    obj_date = document.getElementById("txtDatetime")
+                                    obj_date.value=null
+                                    obj_date.disabled = false
+
+                                    obj_change_date = document.getElementById("txtRadio")
+                                    obj_change_date.disabled = true
+                                    obj_change_date.checked = true
+                                    
+
+                                    obj_reset_date = document.getElementById("txtRadioResetDate")
+                                    obj_reset_date.checked = false
+                                    obj_reset_date.disabled = false
+                                    
+
+                                    obj_info_date = document.getElementById("txtDate_info_obj")
+                                    obj_info_date.value="changed"
+
+
+                                }
+
+                                function CancelDate(){
+                                    obj_date = document.getElementById("txtDatetime")
+                                    obj_date.value=null
+                                    obj_date.disabled = true
+
+                                    obj_change_date = document.getElementById("txtRadio")
+                                    obj_change_date.checked = false
+                                    obj_change_date.disabled = false
+                                    
+                                    
+
+                                    obj_reset_date = document.getElementById("txtRadioResetDate")
+                                    obj_reset_date.checked = true
+                                    obj_reset_date.disabled = true
+
+                                    obj_info_date = document.getElementById("txtDate_info_obj")
+                                    obj_info_date.value="resetted"
+                                }
+
+
+                                function onloadPage(){
+                                    // get the iso time string formatted for usage in an input[\'type="datetime-local"\']
+                                    var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+                                    var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0,-1);
+                                    var localISOTimeWithoutSeconds = localISOTime.slice(0,16);
+
+                                    // select the "datetime-local" input to set the default value on
+                                    var dtlInput = document.querySelector(\'input[type="datetime-local"]\');
+
+                                    // set it and forget it ;)
+                                    dtlInput.value = localISOTime.slice(0,16);
+                                    
+                                    obj_date = document.getElementById("txtDatetime")
+                                    obj_date.value=null
+                                    obj_date.disabled = true
+
+                                }
+                                
+
+
+
+                        </script>';
+                        //echo '<a id="resetDate" onclick="resetDate()">Restaurar data</a>';
+                        echo '<br><br><p>*** Ao alterar uma requisição, esta automaticamente estará sendo aprovada.</p>';
+                    }else if($op_type=="repprove"){
+                        echo '<input type="checkbox" name="txtShouldDelete" id="txtShouldDelete" value="yes"> Deletar requisição';
                     }
-                    else if($info=='wrong_none_date_set'){
-                        echo '<p>A data de agendamento permaneceu a mesma, portanto a requisição não foi alterada.</p>';
-                    }
-                }
-            
-            ?>
-            <input type="hidden" name="_token" value="{{{csrf_token()}}}">
-            <input type="hidden" name="op_type" value="<?= $op_type ?>">
-            <input type="hidden" name="_id" value="<?= $id ?>">
-            Justificativa:
-            <textarea id="txtJustify" name="txtJustify" rows="4" cols="50"></textarea>
-
-            <p>Data de agendamento: <?php echo date('d/m/Y H:i:s', $datetime); ?></p>
-            <?php 
-            
-            
-                if($op_type=='change'){
-                    echo '<input type="datetime-local" id="txtDatetime" name="txtDatetime" onChange="onRestoreDateInfo()">';
-                    echo '<input type="hidden" id="txtDate_info_obj" name="txtDate_info_obj" value="resetted">';
-                    echo '<input type="hidden" id="txtTimestamp" name="txtTimestamp" value="'.$datetime.'">';
-
-                    echo 'Alterar Data<input type="radio" name="txtRadio" id="txtRadio" onclick="ChangeDate()">
-                          Cancelar alteração<input type="radio" name="txtRadioResetDate" id="txtRadioResetDate" onclick="CancelDate()">';
-
-                    echo '<script type="text/javascript">
-                    
-                            function ChangeDate(){
-                                obj_date = document.getElementById("txtDatetime")
-                                obj_date.value=null
-                                obj_date.disabled = false
-
-                                obj_change_date = document.getElementById("txtRadio")
-                                obj_change_date.disabled = true
-                                obj_change_date.checked = true
-                                
-
-                                obj_reset_date = document.getElementById("txtRadioResetDate")
-                                obj_reset_date.checked = false
-                                obj_reset_date.disabled = false
-                                
-
-                                obj_info_date = document.getElementById("txtDate_info_obj")
-                                obj_info_date.value="changed"
-
-
-                            }
-
-                            function CancelDate(){
-                                obj_date = document.getElementById("txtDatetime")
-                                obj_date.value=null
-                                obj_date.disabled = true
-
-                                obj_change_date = document.getElementById("txtRadio")
-                                obj_change_date.checked = false
-                                obj_change_date.disabled = false
-                                
-                                
-
-                                obj_reset_date = document.getElementById("txtRadioResetDate")
-                                obj_reset_date.checked = true
-                                obj_reset_date.disabled = true
-
-                                obj_info_date = document.getElementById("txtDate_info_obj")
-                                obj_info_date.value="resetted"
-                            }
-
-
-                            function onloadPage(){
-                                // get the iso time string formatted for usage in an input[\'type="datetime-local"\']
-                                var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-                                var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0,-1);
-                                var localISOTimeWithoutSeconds = localISOTime.slice(0,16);
-
-                                // select the "datetime-local" input to set the default value on
-                                var dtlInput = document.querySelector(\'input[type="datetime-local"]\');
-
-                                // set it and forget it ;)
-                                dtlInput.value = localISOTime.slice(0,16);
-                                
-                                obj_date = document.getElementById("txtDatetime")
-                                obj_date.value=null
-                                obj_date.disabled = true
-
-                            }
-                            
-
-
-
-                    </script>';
-                    //echo '<a id="resetDate" onclick="resetDate()">Restaurar data</a>';
-                    echo '<p>*** Ao alterar uma requisição, esta automaticamente estará sendo aprovada.</p>';
-                }else if($op_type=="repprove"){
-                    echo '<input type="checkbox" name="txtShouldDelete" id="txtShouldDelete" value="yes"> Deletar requisição';
-                }
-            
-            ?>
-            <input type="submit" value="Realizar operação">
-
-        </form>
-
-
+                
+                ?><br>
+                <input type="submit" class="btn btn-warning" value="Realizar operação">
+            </form>
+        </div>
     </body>
 </html>
--- webapp/resources/views/inspect_req.blade.php
+++ webapp/resources/views/inspect_req.blade.php
@@ -7,76 +7,79 @@
 			  rel="stylesheet">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="/style/estilo.css"/>
+        <link rel="stylesheet" href="/style/estiloInsp.css"/>
         <title>Amicão - Detalhes da requisição</title>
         @include("./user_routesNavigation")
     </head>
-    <body>
+    <body id="grid">
+        <div id="div1">
+            <span class="lNome">Nome:</span> <?= $data->nome ?><br>
+            <span class="lTel">Telefone:</span> <?= $data->phone ?><br>
+            <span class="lEmail">E-mail:</span> <?= $data->email ?><br>
+            <span class="lStatus">Status da Requisição:</span> <?php
+                            
+                            
+                            if($data->status=='not_seen'){
+                                echo 'não avaliada';
+                            }
+                            else if($data->status=='refused'){
+                                echo 'negada';
+                            }
+                            else if($data->status=='acceptted'){
+                                echo 'aceita';
+                            }
 
-        <span class="lNome">Nome:</span> <?= $data->nome ?><br>
-        <span class="lTel">Telefone:</span> <?= $data->phone ?><br>
-        <span class="lEmail">E-mail:</span> <?= $data->email ?><br>
-        <span class="lStatus">Status da Requisição:</span> <?php
-                        
-                        
-                        if($data->status=='not_seen'){
-                            echo 'não avaliada';
-                        }
-                        else if($data->status=='refused'){
-                            echo 'negada';
-                        }
-                        else if($data->status=='acceptted'){
-                            echo 'aceita';
-                        }
+            ?><br>
+            <span class="lData">Data:</span> <?= date('d/m/Y H:i:s', $data->date) ?><br>
+            <span class="lObs">Observações:</span> <?= $data->obs ?><br>
+            <span class="lId">ID (pet):</span> <?= $data->id_pet ?><br>
 
-        ?><br>
-        <span class="lData">Data:</span> <?= date('d/m/Y H:i:s', $data->date) ?><br>
-        <span class="lObs">Observações:</span> <?= $data->obs ?><br>
-        <span class="lId">ID (pet):</span> <?= $data->id_pet ?><br>
+            <?php 
+            $frm_req_change = false;
+            $frm_req_approve = false;
+            $frm_req_refuse = false;
+            if($data->status=='not_seen' || $data->status=='refused'){
+                $frm_req_change = $frm_req_approve = true;
+            }
+            else if($data->status=='acceptted'){
+                $frm_req_change = true;
+                $frm_req_refuse = true;
+            }
+            ?>
+        
+            <?php if($frm_req_change==true):?>
+            <form id="frm_req_change" method="post" action="/institucional/requisicoes/action">
+                <input type="hidden" name="_token" value="{{{csrf_token()}}}">
+                <input type="hidden" name="_id" value="<?= $data->id ?>">
+                <input type="hidden" name="op_type" value="change">
+                <br>
+                <input type="submit" class="btn btn-warning" value="Alterar">
+            </form>
+            <?php endif ?>
 
-        <?php 
-        $frm_req_change = false;
-        $frm_req_approve = false;
-        $frm_req_refuse = false;
-        if($data->status=='not_seen' || $data->status=='refused'){
-            $frm_req_change = $frm_req_approve = true;
-        }
-        else if($data->status=='acceptted'){
-            $frm_req_change = true;
-            $frm_req_refuse = true;
-        }
-        ?>
+            <?php if($frm_req_approve==true):?>
+            <form id="frm_req_approve" method="post" action="/institucional/requisicoes/action">
+                <input type="hidden" name="_token" value="{{{csrf_token()}}}">
+                <input type="hidden" name="_id" value="<?= $data->id ?>">
+                <input type="hidden" name="op_type" value="approve">
+                <br>
+                <input type="submit" class="btn btn-warning" value="Aprovar">
+            </form>
+            <?php endif ?>
 
-        <?php if($frm_req_change==true):?>
-        <form id="frm_req_change" method="post" action="/institucional/requisicoes/action">
-            <input type="hidden" name="_token" value="{{{csrf_token()}}}">
-            <input type="hidden" name="_id" value="<?= $data->id ?>">
-            <input type="hidden" name="op_type" value="change">
-            <input type="submit" class="btn btn-warning" value="Alterar">
-        </form>
-        <?php endif ?>
-
-        <?php if($frm_req_approve==true):?>
-        <form id="frm_req_approve" method="post" action="/institucional/requisicoes/action">
-            <input type="hidden" name="_token" value="{{{csrf_token()}}}">
-            <input type="hidden" name="_id" value="<?= $data->id ?>">
-            <input type="hidden" name="op_type" value="approve">
-            <input type="submit" class="btn btn-warning" value="Aprovar">
-        </form>
-        <?php endif ?>
-
-        <?php if($frm_req_refuse==true):?>
-        <form id="frm_req_refuse" method="post" action="/institucional/requisicoes/action">
-            <input type="hidden" name="_token" value="{{{csrf_token()}}}">
-            <input type="hidden" name="_id" value="<?= $data->id ?>">
-            <input type="hidden" name="op_type" value="repprove">
-            <input type="submit" class="btn btn-warning" value="Reprovar">
-        </form>
-        <?php endif ?>
+            <?php if($frm_req_refuse==true):?>
+            <form id="frm_req_refuse" method="post" action="/institucional/requisicoes/action">
+                <input type="hidden" name="_token" value="{{{csrf_token()}}}">
+                <input type="hidden" name="_id" value="<?= $data->id ?>">
+                <input type="hidden" name="op_type" value="repprove">
+                <br>
+                <input type="submit" class="btn btn-warning" value="Reprovar">
+            </form>
+            <?php endif ?>
 
 
 
-
+        </div>
     </body>
 </html>
 
