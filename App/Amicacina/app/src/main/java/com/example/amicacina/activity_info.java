19b0898302efbe57b64d9fd605eb212df8473cf0
KenichiNishiya <yorimitsu@proton.me>
2022-11-16 07:47:36 -0300

    Removed date and time picker

--- App/Amicacina/app/src/main/java/com/example/amicacina/activity_info.java
+++ App/Amicacina/app/src/main/java/com/example/amicacina/activity_info.java
@@ -33,11 +33,6 @@ import java.util.Calendar;
 import java.util.Locale;
 
 public class activity_info extends AppCompatActivity {
-    private DatePickerDialog datePickerDialog;
-    private Button dateButton;
-    Button timeButton;
-    int hour, minute;
-    String datetime_str, today_date;
     public static final String REQUEST_SENDING_URL = "https://amicao.herokuapp.com/api/application_send/send_request";
     TextView txtMessage;
 
@@ -45,114 +40,10 @@ public class activity_info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        initDatePicker();
-
-        dateButton = findViewById(R.id.datePickerButton);
-        dateButton.setText(getTodaysDate());
-        timeButton = findViewById(R.id.timeButton);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("");
-    }
-
-
-    public void popTimePicker(View view)
-    {
-        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener()
-        {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute)
-            {
-                hour = selectedHour;
-                minute = selectedMinute;
-                timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour, minute));
-            }
-        };
-
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
-
-        timePickerDialog.setTitle("Select Time");
-        timePickerDialog.show();
-    }
-
-
-    private String getTodaysDate()
-    {
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        today_date = makeDateString(day, month, year);
-        return makeDateString(day, month, year);
-    }
-
-    private void initDatePicker()
-    {
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
-        {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day)
-            {
-                month = month + 1;
-                String date = makeDateString(day, month, year);
-                dateButton.setText(date);
-            }
-        };
-
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-
-        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
-
-
-    }
-
-    private String makeDateString( int day, int month, int year)
-    {
-        return  day +  " " + getMonthFormat(month) + " " + year;
-    }
-
-    private String getMonthFormat(int month)
-    {
-        if(month == 1)
-            return "JAN";
-        if(month == 2)
-            return "FEV";
-        if(month == 3)
-            return "MAR";
-        if(month == 4)
-            return "ABR";
-        if(month == 5)
-            return "MAI";
-        if(month == 6)
-            return "JUN";
-        if(month == 7)
-            return "JUL";
-        if(month == 8)
-            return "AGO";
-        if(month == 9)
-            return "SET";
-        if(month == 10)
-            return "OUT";
-        if(month == 11)
-            return "NOV";
-        if(month == 12)
-            return "DEZ";
-
-        return "JAN";
-    }
-
-    public void openDatePicker(View view)
-    {
-        datePickerDialog.show();
     }
 
     public void onClickSend(View view){
@@ -177,9 +68,6 @@ public class activity_info extends AppCompatActivity {
             txtMessage.setText("Uma ação deve ser selecionada!");
         }
 
-        if(timeButton.getText()!=null && timeButton.getText()!=""){
-            datetime_str = today_date + "" + timeButton.getText();
-            Log.d("date", datetime_str);
 
             if(MainActivity.internet_connection){
             Ion.with (activity_info.this)
@@ -189,7 +77,6 @@ public class activity_info extends AppCompatActivity {
                     .setBodyParameter("email", email.getText().toString())
                     .setBodyParameter("phone", telefone.getText().toString())
                     .setBodyParameter("obs", "")
-                    .setBodyParameter("datetime", datetime_str)
                     .setBodyParameter("req_type", req_type)
                     .asJsonObject()
                     .setCallback ( new FutureCallback<JsonObject>() {
@@ -236,13 +123,6 @@ public class activity_info extends AppCompatActivity {
             }else{
                 Toast.makeText(activity_info.this, "Seu dispositivo não tem acesso à internet!", Toast.LENGTH_LONG).show();
             }
-
-        }
-        else{
-            txtMessage.setText("Um horário deve ser selecionado!");
-        }
-
-
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
